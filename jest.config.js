de900b3abb2fb8b708d6f0bfde62bb0dffc05759
Juan Antonio Fernández de Alba <juan@undefinedlabs.com>
2020-02-20 14:46:55 +0100

    add tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testRunner: "@undefinedlabs/scope-agent/jest/testRunner",
+  runner: "@undefinedlabs/scope-agent/jest/runner",
+  setupFilesAfterEnv: ["@undefinedlabs/scope-agent/jest/setupTests"]
 };
